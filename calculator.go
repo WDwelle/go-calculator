2e1d268ecb55ae4e580b23de0327912d4b499171
WDwelle <willdwelle@frontier.com>
2022-03-09 03:39:00 -0800

    added comments

--- calculator.go
+++ calculator.go
@@ -3,9 +3,11 @@ package main
 import "fmt"
 
 func main() {
+	//variable declaration
 	var operator string
 	var number1, number2 int
 
+	//User inputs
 	fmt.Print("Enter first number: ")
 	fmt.Scanln(&number1)
 
@@ -15,33 +17,42 @@ func main() {
 	fmt.Print("Please enter Operator (+,-,/,%,*):")
 	fmt.Scanln(&operator)
 
+	//default value of output
 	output := 0
 
+	//function acts based on operator selected
 	switch operator {
 
+	//addition
 	case "+":
 		output = number1 + number2
 		break
 
+	//subtraction
 	case "-":
 		output = number1 - number2
 		break
 
+	//multiplication
 	case "*":
 		output = number1 * number2
 		break
 
+	//division
 	case "/":
 		output = number1 / number2
 		break
 
+	//remainder
 	case "%":
 		output = number1 % number2
 		break
 
+	//default if missing entry
 	default:
 		fmt.Println("Invalid Operation")
 	}
 
+	//print result
 	fmt.Printf("%d %s %d = %d", number1, operator, number2, output)
 }
